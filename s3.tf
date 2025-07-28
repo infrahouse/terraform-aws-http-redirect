40194634e6a1b03d74bfed3ffd59b9ef7a39d5a8
Oleksandr Kuzminskyi <1763754+akuzminsky@users.noreply.github.com>
2025-07-28 13:51:54 -0700

    Enforce TLS on requests to S3 (#6)

--- s3.tf
+++ s3.tf
@@ -19,3 +19,36 @@ resource "aws_s3_bucket_public_access_block" "redirect" {
   ignore_public_acls      = true
   restrict_public_buckets = true
 }
+
+data "aws_iam_policy_document" "enforce_ssl_policy" {
+  statement {
+    sid    = "AllowSSLRequestsOnly"
+    effect = "Deny"
+
+    actions = [
+      "s3:*",
+    ]
+
+    resources = [
+      aws_s3_bucket.redirect.arn,
+      "${aws_s3_bucket.redirect.arn}/*",
+    ]
+
+    principals {
+      type        = "*"
+      identifiers = ["*"]
+    }
+
+    condition {
+      test     = "Bool"
+      variable = "aws:SecureTransport"
+      values   = ["false"]
+    }
+  }
+
+}
+
+resource "aws_s3_bucket_policy" "redirect" {
+  bucket = aws_s3_bucket.redirect.id
+  policy = data.aws_iam_policy_document.enforce_ssl_policy.json
+}
