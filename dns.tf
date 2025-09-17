00ca23992acc96a255ffac2c40838549dba713fa
Maksym Kryva <maksym@tinyfish.io>
2025-09-17 11:11:24 +0300

    add caa record (#7)
    * add caa record
    
    * Update dns.tf
    
    Signed-off-by: Maksym Kryva <maksym@tinyfish.io>
    
    ---------
    
    Signed-off-by: Maksym Kryva <maksym@tinyfish.io>

--- dns.tf
+++ dns.tf
@@ -9,3 +9,15 @@ resource "aws_route53_record" "extra" {
     zone_id                = aws_cloudfront_distribution.redirect.hosted_zone_id
   }
 }
+
+resource "aws_route53_record" "caa_record" {
+  count   = length(var.redirect_hostnames)
+  zone_id = var.zone_id
+  name    = trimprefix(join(".", [var.redirect_hostnames[count.index], data.aws_route53_zone.redirect.name]), ".")
+  type    = "CAA"
+  ttl     = 300
+  records = [
+    "0 issue \"amazon.com\"",
+    "0 issuewild \";\""
+  ]
+}
